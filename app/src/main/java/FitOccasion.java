a404a0396f8124981e109409b87950661c7e6c34
bederman <bederman.evan@gmail.com>
2015-05-10 17:03:36 -0500

    Changed Occasion to an interface, added CalOccasion and FitOccasion classes.

---
+++ app/src/main/java/FitOccasion.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by evan on 5/10/15.
+ */
+public class FitOccasion implements Occasion{
+
+    @Override
+    public Date getDate() {
+        return null;
+    }
+
+    @Override
+    public long getDuration() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<String> getGuests() {
+        return null;
+    }
+
+    @Override
+    public String getService() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public Object getLocation() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getTags() {
+        return null;
+    }
+
+    @Override
+    public void setTags(ArrayList<String> newtags) {
+
+    }
+
+    @Override
+    public void addTag(String tag) {
+
+    }
+}
