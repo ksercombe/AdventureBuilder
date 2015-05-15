2145433c85b584d787ffdedf65ef238e7a97f152
ksercombe <ksercombe519@gmail.com>
2015-05-15 12:40:58 -0500

    Adding Classes

---
+++ app/src/main/java/Generator.java
@@ -0,0 +1,17 @@
+/**
+ * Created by katesercombe on 5/14/15.
+ */
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Generator {
+
+String fragMatch(Occasion occ, ArrayList<StoryFrag> allFrags){
+    return "Wooooo";
+}
+
+String storyTeller(ArrayList<StoryFrag> frags, ArrayList<Occasion> occasions){
+    return "Wooooo";
+}
+
+}
